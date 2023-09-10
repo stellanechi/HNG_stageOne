98451a0d2fceaa3eacbc35f7711dd462be0da18a
stellanechi <stellanechi65@gmail.com>
2023-09-10 09:21:00 +0100

    real time integration

--- STAGEONE/script.js
+++ STAGEONE/script.js
@@ -1,9 +1,4 @@
-const day = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-
-console.log(day);
-
-const currentDate = new Date();
-
+function newDate() {
 const daysOfWeek = [
   "Sunday",
   "Monday",
@@ -14,4 +9,17 @@ const daysOfWeek = [
   "Saturday",
 ];
 
-day.textContent = daysOfWeek[currentDate.getDay()];
+   const date = new Date();
+   const currentTime = date.toISOString().slice(11, 19);
+   const realDay = daysOfWeek[date.getUTCDay()];
+   
+   const day = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+   const time = document.querySelector('[ data-testid="currentUTCTime"]');
+   day.textContent = realDay;
+   time.textContent = currentTime;
+   setTimeout(newDate, 1000);
+}
+newDate();
+
+
+
